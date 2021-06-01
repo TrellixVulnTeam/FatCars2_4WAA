d5f63b067b20ed7a014727e4d95b450357af87fe
JJPontes <50496581+JJPontes@users.noreply.github.com>
2021-05-31 21:38:00 -0300

    Inicio Angular

---
+++ front/FatCars-App/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './Users/Users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+      UsersComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
